f679e29b1097d204cc8256c8ad9c300b0bbea094
Vlad <gaad.ru@yandex.ru>
2021-03-15 12:44:00 +0500

    Filling controllers for Product and Supplier

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,29 +4,58 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Product\ProductCollection;
+use App\Http\Resources\Product\ProductResource;
 use Illuminate\Http\Request;
+use App\Http\Requests\ProductRequest;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
+
+    /**
+     * Restriction on pagination
+     * @var int
+     */
+    public const LIMIT_PAGINATION = 100;
+
+    /**
+     * Server response code for invalid request
+     * @var int
+     */
+    protected $responceToBadRequest = Response::HTTP_BAD_REQUEST;
+
+    /**
+     * Server response code with empty response body
+     * @var int
+     */
+    protected $responceWithoutBody = Response::HTTP_NO_CONTENT;
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(int $limit = self::LIMIT_PAGINATION, int $offset = 0)
     {
-        //
+
+        if ($limit > self::LIMIT_PAGINATION) {
+            $limit = self::LIMIT_PAGINATION;
+        } 
+        
+        $data = DB::table('products')
+            ->select('products.id', 'products.name_product', 'products.description_product', 'products.cost_product', 'suppliers.name_supplier')
+            ->join('suppliers', 'suppliers.id', '=', 'products.supplier_id')
+            ->limit($limit)
+            ->offset($offset)
+            ->orderBy('products.id')
+            ->get();
+
+        return new ProductCollection($data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -34,9 +63,21 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product;
+        $product->name_product = $request->name_product;
+        $product->description_product = $request->description_product;
+        $product->supplier_id = $request->supplier_id;
+        $product->cost_product = $request->cost_product;
+        if ($product->save()) {
+            return response([
+                'status' => 'created',
+                'code' => Response::HTTP_CREATED
+            ], Response::HTTP_CREATED);
+        } else {
+            return response('', $this->responceToBadRequest);
+        }
     }
 
     /**
@@ -47,19 +88,15 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        // $nameSupplier = DB::table('products')
+        //         ->where('products.id', '=', $product['id'])
+        //         ->select(DB::raw('suppliers.name_supplier'))
+        //         ->join('suppliers', 'suppliers.id', '=', 'products.supplier_id')
+        //         ->get();
+        // $product['nameSupplier'] = $nameSupplier[0]->name_supplier;
+        // return new ProductResource($product);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Product $product)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -70,7 +107,11 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        if ($product->update($request->all())) {
+            return response('', $this->responceWithoutBody);
+        } else {
+            return response('', $this->responceToBadRequest);
+        }
     }
 
     /**
@@ -81,6 +122,36 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        if ($product->delete()) {
+            return response('', $this->responceWithoutBody);
+        } else {
+            return response('', $this->responceToBadRequest);
+        }
     }
+
+    
+    /**
+     *  Sort Result Query Asc
+     * @return
+     */
+    public function sort(string $nameSupplier, string $sortingType)
+    {
+        $data = DB::table('products')
+                ->select(DB::raw('products.id, products.name_product, products.description_product, products.cost_product, suppliers.name_supplier'))
+                ->join('suppliers', 'suppliers.id', '=', 'products.supplier_id')
+                ->where('suppliers.name_supplier', '=', $nameSupplier)
+                ->orderBy('products.cost_product', $sortingType)
+                ->get();
+        /**SELECT products.id, products.name_product, products.description_product, products.cost_product, suppliers.name_supplier
+        FROM `products`
+        INNER JOIN `suppliers`  ON suppliers.id = products.supplier_id
+        WHERE suppliers.name_supplier = $nameSupplier
+        GROUP BY products.supplier_id 
+        ORDER BY products.cost_product $sortingType
+        */
+
+        return new ProductCollection($data);
+    }
+
+    
 }
--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -4,29 +4,56 @@ namespace App\Http\Controllers;
 
 use App\Models\Supplier;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Supplier\SupplierCollection;
+use App\Http\Resources\Supplier\SupplierResource;
 use Illuminate\Http\Request;
+use App\Http\Requests\SupplierRequest;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\Response;
 
 class SupplierController extends Controller
 {
+    /**
+     * Restriction on pagination
+     * @var int
+     */
+    public const LIMIT_PAGINATION = 100;
+
+    /**
+     * Server response code for invalid request
+     * @var int
+     */
+    protected $responceToBadRequest = Response::HTTP_BAD_REQUEST;
+
+    /**
+     * Server response code with empty response body
+     * @var int
+     */
+    protected $responceWithoutBody = Response::HTTP_NO_CONTENT;
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(int $limit = self::LIMIT_PAGINATION, int $offset = 0)
     {
-        //
+
+        if ($limit > self::LIMIT_PAGINATION) {
+            $limit = self::LIMIT_PAGINATION;
+        } 
+
+        $data = DB::table('suppliers')
+            ->select('suppliers.id', 'suppliers.name_supplier', DB::raw('count(*) AS total_products'))
+            ->join('products', 'suppliers.id', '=', 'products.supplier_id')
+            ->groupBy('products.supplier_id')
+            ->limit($limit)
+            ->offset($offset)
+            ->orderBy('suppliers.id')
+            ->get();
+        return new SupplierCollection($data);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -34,9 +61,18 @@ class SupplierController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(SupplierRequest $request)
     {
-        //
+        $supplier = new Supplier;
+        $supplier->name_supplier = $request->name_supplier;
+        if ($supplier->save()) {
+            return response([
+                'status' => 'created',
+                'code' => Response::HTTP_CREATED
+            ], Response::HTTP_CREATED);
+        } else {
+            return response('', $this->responceToBadRequest);
+        }
     }
 
     /**
@@ -46,20 +82,17 @@ class SupplierController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Supplier $supplier)
-    {
-        //
+    {   
+        $dataOnRequestTotalProducts = DB::table('suppliers')
+                ->select(DB::raw('count(*) AS total_products'))
+                ->join('products', 'suppliers.id', '=', 'products.supplier_id')
+                ->where('suppliers.id', '=', $supplier['id'])
+                ->groupBy('products.supplier_id')
+                ->get();
+        $supplier['total_products'] = $dataOnRequestTotalProducts[0]->total_products;
+        return new SupplierResource($supplier);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Supplier  $supplier
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Supplier $supplier)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -70,7 +103,11 @@ class SupplierController extends Controller
      */
     public function update(Request $request, Supplier $supplier)
     {
-        //
+        if ($supplier->update($request->all())) {
+            return response('', $this->responceWithoutBody);
+        } else {
+            return response('', $this->responceToBadRequest);
+        }
     }
 
     /**
@@ -81,6 +118,11 @@ class SupplierController extends Controller
      */
     public function destroy(Supplier $supplier)
     {
-        //
+        if ($supplier->delete()) {
+            return response('', $this->responceWithoutBody);
+        } else {
+            return response('', $this->responceToBadRequest);
+        }
     }
+
 }
