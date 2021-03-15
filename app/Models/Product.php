3d185591b89d26be34844a9c130676bd95a4d8a7
Vlad <gaad.ru@yandex.ru>
2021-03-15 13:14:09 +0500

    Specified fields for POST and PUT requests

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name_product','description_product','cost_product','supplier_id'
+    ];
 }
